cb014f8b254cd1abddad979a57e989ca74b6ef5f
HIDE <48782538+HIDE810@users.noreply.github.com>
2020-05-14 00:13:30 +0900

    Remove the date

--- assets/js/time.js
+++ assets/js/time.js
@@ -13,17 +13,10 @@ function set2fig(num) {
 
 function getNow() {
 	var now = new Date();
-	var year = now.getFullYear();
-	var mon = now.getMonth()+1;
-	var day = now.getDate();
 	var hour = set2fig(now.getHours());
 	var min = set2fig(now.getMinutes());
 	var sec = set2fig(now.getSeconds());
-    var dayOfWeek = now.getDay();
-    
-    var monStr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][mon];
-    var dayOfWeekStr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][dayOfWeek];
 
-	var s = monStr + " " + day + ", " + year + " (" + dayOfWeekStr + ")<br>" + hour + ":" + min + ":" + sec; 
+	var s = hour + ":" + min + ":" + sec; 
 	return s;
 }
